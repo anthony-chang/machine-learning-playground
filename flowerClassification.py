e861aab6ca1c23ac5a3cb22acaa0ae831e604a93
anthony-chang <54450499+anthony-chang@users.noreply.github.com>
2020-01-22 17:04:30 -0500

    plot data

--- flowerClassification.py
+++ flowerClassification.py
@@ -22,7 +22,25 @@ names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = read_csv(url, names=names)
 
 # shape
-print(dataset.shape)
+# print(dataset.shape)
 
 # head
-print(dataset.head(20)) # first 20 lines of dataset
+# print(dataset.head(20)) # first 20 lines of dataset
+
+# print descriptions
+# print(dataset.describe())
+
+# class distribution
+# print(dataset.groupby('class').size())
+
+# box and whisker plots
+dataset.plot(kind = 'box', subplots = True, layout = (2, 2), sharex = False, sharey = False)
+pyplot.show()
+
+# histogram plots   
+dataset.hist()
+pyplot.show()
+
+# scatter plot matrix
+scatter_matrix(dataset)
+pyplot.show()
